de6a6d0947246582dea313693d784152ece26c7b
neha-python <150993194+nehasri4@users.noreply.github.com>
2024-07-24 10:36:41 +0530

    defaultvalues.java

---
+++ defaultvalues.java
@@ -0,0 +1,20 @@
+//java progarm to display default values of all primitive data type 
+import java.util.Scanner;
+public class defaultvalues{
+	static boolean bool;
+	static byte b;
+	static short s;
+	static int i;
+	static long l;
+	static float f;
+	static char c;
+	public static void main(String[] args){
+		System.out.println("default values of primitive data type");
+		System.out.println("boolean "+bool);
+		System.out.println("byte "+b);
+		System.out.println("short "+s);
+		System.out.println("int "+i);
+		System.out.println("float "+f);
+		System.out.println("char "+c);
+		}
+}
