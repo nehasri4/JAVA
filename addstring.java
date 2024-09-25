7072af098c5c1704604baa866ead53b2603461f5
neha-python <150993194+nehasri4@users.noreply.github.com>
2024-09-25 18:42:48 +0530

    Create addstring.java

---
+++ addstring.java
@@ -0,0 +1,8 @@
+public class Main{
+	public static void main(String[]args){
+    	String firstname="Neha";
+        String secondname="sri";
+        String fullname= firstname+secondname;
+        System.out.println(fullname);
+    }
+}
