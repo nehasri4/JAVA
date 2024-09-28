ca1848fca96d5184831cda4c27b0f00a1d2c462a
neha-python <150993194+nehasri4@users.noreply.github.com>
2024-09-28 15:28:02 +0530

    Create UpperLowercase.java

---
+++ UpperLowercase.java
@@ -0,0 +1,8 @@
+//UPPERCASE AND LOWERCASE
+public class Main{
+	public static void main(String[]args){
+    	String txt= "nothing";
+        System.out.println(txt.toUpperCase());
+        System.out.println(txt.toLowerCase());
+    }
+}
