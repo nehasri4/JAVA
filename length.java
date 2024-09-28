46e5e2ae161ff771ce2f52d5313bae37b87683c3
neha-python <150993194+nehasri4@users.noreply.github.com>
2024-09-28 15:40:36 +0530

    Create length.java

---
+++ length.java
@@ -0,0 +1,7 @@
+//find the length of the srting
+public class Main{
+	public static void main(String[]args){
+    	String smily = "nothingisimpossibletheworditselfsaysiampossible";
+    	System.out.print("length of the string: " + smily.length());
+    }
+}
