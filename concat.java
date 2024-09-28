81e3541eb9364eee7bb57cc7b505011b1d4ad2e1
neha-python <150993194+nehasri4@users.noreply.github.com>
2024-09-28 16:14:43 +0530

    Create concat.java

---
+++ concat.java
@@ -0,0 +1,8 @@
+//add two strings using concat
+public class Main{
+	public static void main(String[] args){
+    	String first="smily";
+        String second="angel";
+    	System.out.println(first.concat(second));
+    }
+}
