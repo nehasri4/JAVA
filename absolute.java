9825fad29693e6f8821315b91bd7d10db3a6fe6a
neha-python <150993194+nehasri4@users.noreply.github.com>
2024-10-07 15:37:15 +0530

    Create absolute.java

---
+++ absolute.java
@@ -0,0 +1,6 @@
+//The Math.abs(x) method returns the absolute (positive) value of x:
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(Math.abs(-4.7));  
+  }
+}
